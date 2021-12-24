64146808788a2d5d40f04a546ea77435cb07df93
Nick <65750313+nkanders@users.noreply.github.com>
2021-12-23 16:17:26 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { GLTFModel, AmbientLight, DirectionLight } from "react-3d-viewer";
+import "./styles.css";
+
+export default function App() {
+  const modelPath =
+    "https://raw.githubusercontent.com/dwqdaiwenqi/react-3d-viewer/master/site/src/lib/model/DamagedHelmet.gltf";
+  return (
+    <div>
+      Aditya
+      <GLTFModel src={modelPath}>
+        <AmbientLight color={0xffffff} />
+        <DirectionLight
+          color={0xffffff}
+          position={{ x: 100, y: 200, z: 100 }}
+        />
+        <DirectionLight
+          color={0xff00ff}
+          position={{ x: -100, y: 200, z: -100 }}
+        />
+      </GLTFModel>
+    </div>
+  );
+}
